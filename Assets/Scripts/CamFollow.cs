13e6f698f003a0c5cedc0fbaf0d1f0f651d5e977
SlowMO_Desktop <moritz-schuetz@gmx.de>
2020-09-15 21:57:11 +0200

    Version 0.0.2
    Added Zoom

--- Assets/Scripts/CamFollow.cs
+++ Assets/Scripts/CamFollow.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class CamFollow : MonoBehaviour
 {
+    public float followModifier;
+
+    private Rigidbody rb;
     private Camera cam;
     void Start()
     {
         cam = GetComponent<Camera>();
+        rb = this.GetComponentInParent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        Rigidbody spartanRb = this.GetComponentInParent<Rigidbody>();
-        Vector3 wishedCamPos = new Vector3(0+Input.GetAxis("Horizontal")*2, 0- Input.GetAxis("Vertical")*2, 20);
+    {   
+
+        Vector3 wishedCamPos = new Vector3(0 + Input.GetAxis("Horizontal") * followModifier, 0 - Input.GetAxis("Vertical") * followModifier, 20);
         this.transform.localPosition = wishedCamPos;
+        
     }
 }
